5cf086b18cc8a54c42f4836d6cc3f7cfd3b5ff1b
Reshad Hasan <reshad.kinetik.care>
2020-11-27 15:22:29 +0600

    client connect disconnect login in useEffect

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(()=>{
+    const data = queryString.parse(location.search);
+    setName(data.name);
+    setRoom(data.room);
+
+    socket = io(ENDPOINT);
+    socket.emit('join', {name, room}, ({})=> {});
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  },[ENDPOINT, location.search]);
   return (
     <h1>Chat</h1>
   );
